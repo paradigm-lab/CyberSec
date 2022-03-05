7081dfdeb9d851f5477b6d7d8b8e6c5649c4ef0e
collins <collinsboniface1000@gmail.com>
2022-03-05 23:34:41 +0300

    Created a new method to check if the ip is 0 or user has got input of the ip address

--- Mac_changer/findallip.py
+++ Mac_changer/findallip.py
@@ -4,4 +4,12 @@ import nmap
 class Network(object):
     def __init__(self):
         ip = input("Please Enter IP Default is 192.168.1.1/192.168.0.1: ")
-        self.ip = ip`
+        self.ip = ip
+
+    def networkScanner(self):
+        if len(self.ip) == 0:
+            network = '192.168.1.1/24'
+        else:
+            network = self.ip + '/24'
+
+        print("Scanning Please wait......")
