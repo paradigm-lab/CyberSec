2045405493317b67e5ee1381c5c06d8de6864cba
collins <collinsboniface1000@gmail.com>
2022-03-07 16:10:43 +0300

    Created a function to change the mac Address

--- Mac_changer/macAddress.py
+++ Mac_changer/macAddress.py
@@ -34,3 +34,11 @@ def get_arguments():
     return options
 
 
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+
