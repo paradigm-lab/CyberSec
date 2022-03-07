fef6354f95f26afce11f7ee5c5836e4694667845
collins <collinsboniface1000@gmail.com>
2022-03-07 15:10:58 +0300

    Imported the library to be used in my program and give the description of the Mac Address and the librarys to be used

--- Mac_changer/macAddress.py
+++ Mac_changer/macAddress.py
@@ -1,3 +1,19 @@
 import subprocess
+import optparse
+import re
 
-subprocess.call(["ifconfig", "wlan0", "inet", "192.168.43.78"])
+# MAC Changer or Physical Address
+# MAC Stands for (Media Access Controlle)
+
+# Every Device has a uniq Mac-Address
+# First three part of the Address identify the "Device"
+# Last three part of the Address identify the "Manufacture of the Device"
+# Mac-Address Spoofing
+
+
+# subprocess enables us to run ystem commands
+# optparse used to parse arguments in our program such as the Mac-Address and interface
+# re used for the Regular Expression
+
+
+
